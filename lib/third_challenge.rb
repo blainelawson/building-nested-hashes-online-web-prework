12e67562950d427fb86e0e95828b5d53db5576ef
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-13 16:32:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,15 +8,30 @@ def third_challenge
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
-        },
+      },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
+      },
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
         },
-      :hero_friends => []
-   }, 
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
+      ]
+    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
+      }
         },
       :matriarch => {
         #your key/value pairs here
